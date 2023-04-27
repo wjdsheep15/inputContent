b29d25404fb6adcd37277c3586056d0d0f0df625
JungSeungGeun <wjd15sheep@naver.com>
2023-04-27 18:08:18 +0900

    inputcontent

---
+++ app/src/main/java/com/example/inputcontent2/inputContent.kt
@@ -0,0 +1,11 @@
+package com.example.inputcontent2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class inputContent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_content)
+    }
+}
